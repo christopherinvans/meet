9d1447453281ec3675549e80cb5e293804c36270
christopherinvans <106615358+christopherinvans@users.noreply.github.com>
2023-03-08 15:22:40 -0800

    Revert "rework city search"
    This reverts commit cf8752a7f2e76d4d088dd53accc12b983f159b5b.

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,87 +1,45 @@
-import React, { Component } from "react";
-import { InfoAlert } from "./Alert";
+import React, { Component } from 'react';
 
 class CitySearch extends Component {
-  state = {
-    query: "",
-    suggestions: [],
-    showSuggestions: false,
-    infoText: "",
-  };
-
-  handleInputChanged = (event) => {
-    const value = event.target.value;
-    this.setState({ showSuggestions: true });
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    if (suggestions.length === 0) {
-      this.setState({
-        query: value,
-        infoText:
-          "We cannot find the city you are looking for. Please try another city",
-      });
-    } else {
-      return this.setState({
-        query: value,
-        suggestions,
-        infoText: "",
-      });
-    }
-  };
-
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-      suggestions: [],
-      showSuggestions: false,
-      infoText: "",
-    });
-    this.props.updateEvents(suggestion);
-  };
-
-  getSuggestionsStyle = () => {
-    return this.state.showSuggestions
-      ? {
-          top: this.state.infoText === "" ? "48px" : "70px",
-        }
-      : { opacity: 0 };
-  };
-
-  hideSuggestions = () => {
-    this.setState({
-      showSuggestions: false,
-    });
-  };
+    state = {
+        query: '',
+        suggestions: [],
+        showSuggestions: undefined
+      }
+      handleInputChanged = (event) => {
+        const value = event.target.value;
+        const suggestions = this.props.locations.filter((location) => {
+          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        });
+        this.setState({
+          query: value,
+          suggestions,
+        });
+      };
+      handleItemClicked = (suggestion) => {
+        this.setState({
+          query: suggestion,
+          showSuggestions: false
+        });
+      
+        this.props.updateEvents(suggestion);
+      }
 
   render() {
     return (
-      <div className="CitySearch" onBlur={this.hideSuggestions}>
-        <InfoAlert text={this.state.infoText} />
-        <input
-          type="text"
-          className="city"
-          value={this.state.query}
-          onChange={this.handleInputChanged}
-          placeholder="Search for location..."
-          onFocus={() => {
-            this.setState({ showSuggestions: true });
-          }}
-        />
-        <ul className="suggestions" style={this.getSuggestionsStyle()}>
-          {this.state.suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              onClick={() => this.handleItemClicked(suggestion)}
-            >
-              {suggestion}
-            </li>
-          ))}
-          <li onClick={() => this.handleItemClicked("all")}>
-            <b>See all cities</b>
-          </li>
-        </ul>
-      </div>
+        <div className="CitySearch">
+      <input
+        type="text"
+         className="city"
+        value={this.state.query}
+        onChange={this.handleInputChanged}
+        onFocus={() => { this.setState({ showSuggestions: true }) }}
+      />
+<ul className="suggestions" style={this.state.showSuggestions ? {}: { display: 'none' }}>
+  {this.state.suggestions.map((suggestion) => (<li onClick={() => this.handleItemClicked(suggestion)}>{ suggestion }</li>))}
+  <li onClick={() => this.handleItemClicked("all")}><b>See all cities</b></li>
+</ul>
+    </div>
     );
   }
 }
